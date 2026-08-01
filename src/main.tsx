959b216f3f5ea67938873cf3db3fff45987e8dd6
Bigode <paulo.eduardo.web@gmail.com>
2026-08-01 16:40:43 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('[APP BUILD VERSION]', '2026-08-01-final-disconnect-v1');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
