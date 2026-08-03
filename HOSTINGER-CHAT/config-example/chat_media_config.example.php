e31a23ec3bc656d0f97c70c230afe3d0f279cbd1
Bigode <paulo.eduardo.web@gmail.com>
2026-08-03 00:26:49 -0300

    Add files via upload

---
+++ HOSTINGER-CHAT/config-example/chat_media_config.example.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Exemplo de configuração de segredo de mídia para Hostinger
+ * Salve este arquivo fora de public_html no caminho:
+ * /home/SEU_USUARIO_HOSTINGER/config/chat_media_config.php
+ */
+return [
+    'upload_secret' => 'COLOQUE_AQUI_UM_SEGREDO_DE_PELO_MENOS_64_CARACTERES'
+];
