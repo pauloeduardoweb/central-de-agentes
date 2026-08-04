528c30a2f2bd73a447045603631842a46b245a37
Bigode <paulo.eduardo.web@gmail.com>
2026-08-03 21:47:18 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,29 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-console.log('[APP BUILD VERSION]', '2026-08-01-final-disconnect-v1');
+const APP_BUILD_VERSION = '2026-08-03-v3-android-fix';
+console.log('[APP BUILD VERSION]', APP_BUILD_VERSION);
+
+// Purge old ServiceWorker registrations and browser caches on Android/iOS
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister().then((unregistered) => {
+        if (unregistered) {
+          console.log('[SW CLEANUP] Unregistered legacy service worker');
+        }
+      });
+    }
+  });
+
+  if ('caches' in window) {
+    caches.keys().then((names) => {
+      for (const name of names) {
+        caches.delete(name);
+      }
+    });
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
